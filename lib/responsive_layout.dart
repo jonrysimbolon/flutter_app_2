8563bb563e9d3db99a977aba73a23faaa7d68ecb
jonrysimbolon <jonrysimbolon19@gmail.com>
2023-09-15 09:55:01 +0700

    Memisah 2 layar(30%, 70%) - responsive

--- lib/responsive_layout.dart
+++ lib/responsive_layout.dart
@@ -6,26 +6,57 @@ class ElevenScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
-    Orientation orientation = MediaQuery.of(context).orientation;
 
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Text(
-            'Screen width: ${screenSize.width.toStringAsFixed(2)}',
-            style: const TextStyle(color: Colors.white, fontSize: 18),
-            textAlign: TextAlign.center,
-          ),
-          Text(
-            'Orientation: $orientation',
-            style: const TextStyle(color: Colors.white, fontSize: 18),
-            textAlign: TextAlign.center,
-          )
-        ],
-      ),
-    );
+        backgroundColor: Colors.blueGrey,
+        body: Row(
+          children: [
+            Expanded(child: LayoutBuilder(builder: (context, constraints) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    'Screen width: ${screenSize.width.toStringAsFixed(2)}', // asFixed(2), membuat hanya ada 2 angka dibelang koma
+                    style: const TextStyle(color: Colors.white, fontSize: 18),
+                    textAlign: TextAlign.center,
+                  ),
+                  Text(
+                    'LayoutBuilder: ${constraints.maxWidth}',
+                    style: const TextStyle(color: Colors.white, fontSize: 18),
+                    textAlign: TextAlign.center,
+                  )
+                ],
+              );
+            })),
+            Expanded(
+                flex: 3,
+                child: LayoutBuilder(
+                  builder: (context, constraint) {
+                    return Container(
+                      color: Colors.white,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Text(
+                            'MediaQuery: ${screenSize.width.toStringAsFixed(2)}',
+                            style: const TextStyle(
+                                color: Colors.blueGrey, fontSize: 18),
+                            textAlign: TextAlign.center,
+                          ),
+                          Text(
+                            'LayoutBuilder: ${constraint.maxWidth}',
+                            style: const TextStyle(
+                                color: Colors.blueGrey, fontSize: 18),
+                            textAlign: TextAlign.center,
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                ))
+          ],
+        ));
   }
 }

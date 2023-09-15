9314df4bd9f3cac0d0c42e41f8f9759fcac3bffa
jonrysimbolon <jonrysimbolon19@gmail.com>
2023-09-15 08:02:03 +0700

    dinamis data dengan listview

--- lib/listview_screen.dart
+++ lib/listview_screen.dart
@@ -8,65 +8,27 @@ class EightScreen extends StatefulWidget {
 }
 
 class _EightScreen extends State<EightScreen> {
+  final List<int> numberList = const <int>[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-          children: [
-            Container(
-              height: 250,
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                border: Border.all(color: Colors.black),
-              ),
-              child: const Center(
-                child: Text(
-                  '1',
-                  style: TextStyle(fontSize: 50),
-                ),
-              ),
+        body: ListView(
+      children: numberList.map((number) {
+        return Container(
+          height: 250,
+          decoration: BoxDecoration(
+            color: Colors.grey,
+            border: Border.all(color: Colors.black),
+          ),
+          child: Center(
+            child: Text(
+              '$number',
+              style: const TextStyle(fontSize: 50),
             ),
-            Container(
-              height: 250,
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                border: Border.all(color: Colors.black),
-              ),
-              child: const Center(
-                child: Text(
-                  '2',
-                  style: TextStyle(fontSize: 50),
-                ),
-              ),
-            ),
-            Container(
-              height: 250,
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                border: Border.all(color: Colors.black),
-              ),
-              child: const Center(
-                child: Text(
-                  '3',
-                  style: TextStyle(fontSize: 50),
-                )
-                ),
-            ),
-            Container(
-              height: 250,
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                border: Border.all(color: Colors.black),
-              ),
-              child: const Center(
-                child: Text(
-                  '4',
-                  style: TextStyle(fontSize: 50),
-                ),
-              ),
-            )
-          ]
-      )
-    );
+          ),
+        );
+      }).toList(),
+    ));
   }
 }

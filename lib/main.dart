d71d31a507d82f22f3e6b5a636bc20885304ba17
jonrysimbolon <jonrysimbolon19@gmail.com>
2023-09-15 09:40:17 +0700

    MediaQuery

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,8 @@ import 'package:flutter/material.dart';
 //import 'package:flutter_app_2/font_component.dart';
 //import 'package:flutter_app_2/listview_screen.dart';
 //import 'package:flutter_app_2/expanded_and_flexible_widget.dart';
-import 'package:flutter_app_2/navigation_component.dart';
+//import 'package:flutter_app_2/navigation_component.dart';
+import 'package:flutter_app_2/responsive_layout.dart';
 
 void main() {
   runApp(const MyApp());
@@ -34,7 +35,8 @@ class MyApp extends StatelessWidget {
       //home: const SevenScreen(),
       //home: const EightScreen(),
       //home: const NineScreen(),
-      home: const TenScreen(),
+      //home: const TenScreen(),
+      home: const ElevenScreen(),
     );
   }
 }
